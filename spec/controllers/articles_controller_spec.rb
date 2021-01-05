f2f948579d8aa3b5422b04542457c4e0002b891c
sonyavera <sonyavgould@gmail.com>
2021-01-05 12:57:30 -0700

    articles controller refactor

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -2,28 +2,22 @@ require 'rails_helper'
 
 describe ArticlesController do
     describe '#index' do
+        subject { get :index}
         it 'should return success response' do
-            get :index
+            subject
             expect(response).to have_http_status(:ok)
         end
 
         it 'should return proper json' do
-            create_list :article, 2
-            get :index
-            json = JSON.parse(response.body)
-            pp json
-            json_data = json['data']
-            expect(json_data.length).to eq(2)
-            expect(json_data[0]['attributes']).to eq({
-                "title" => "Article 1",
-                "content" => "Content of article 1",
-                "slug" => "article-1"
+            articles = create_list :article, 2
+            subject
+            articles.each_with_index do |article, index|
+            expect(json_data[index]['attributes']).to eq({
+                "title" => article.title,
+                "content" => article.content,
+                "slug" => article.slug
             })
-            expect(json_data[1]['attributes']).to eq({
-                "title" => "Article 2",
-                "content" => "Content of article 2",
-                "slug" => "article-2"
-            })
+            end
         end
     end
 end
